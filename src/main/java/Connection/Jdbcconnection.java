bfb7c7a4b1ceba6e18aaa84ef9644f9c01a1a021
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-20 19:37:19 +0330

    add Jdbcconnection codes

--- src/main/java/Connection/Jdbcconnection.java
+++ src/main/java/Connection/Jdbcconnection.java
@@ -1,4 +1,27 @@
 package Connection;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Jdbcconnection {
+
+    private static final Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "#HDn1998");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Jdbcconnection() {
+    }
+
+    static public Connection getConnection() {
+        return connection;
+    }
+
 }
