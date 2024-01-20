eb981b9fdbd01f6f955e0418b880f2474537a0b6
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-20 12:07:16 +0330

    add fields such ass construnctors and setter getter and tostring in user class

--- src/main/java/Entity/User.java
+++ src/main/java/Entity/User.java
@@ -1,4 +1,19 @@
 package Entity;
 
+import lombok.*;
+
+
+import java.util.PrimitiveIterator;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class User {
+    private int id;
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
 }
