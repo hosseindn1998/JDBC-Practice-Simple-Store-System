fe093d7734ed7c27fba45f2ea0d1abd494ee65d6
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-20 20:31:36 +0330

    CREATE all tables(user shareholder brand and ...)

---
+++ src/main/java/Sql/table.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS Users(
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS Categories(
+  category_id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS Brand(
+     brand_id SERIAL PRIMARY KEY,
+     name VARCHAR(255),
+     website VARCHAR(255),
+     description VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS Product(
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    create_date VARCHAR(255),
+    category_id INTEGER,
+    brand_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id),
+    FOREIGN KEY (brand_id) REFERENCES Brand(brand_id)
+);
+
+CREATE TABLE IF NOT EXISTS Shareholder(
+    shareholder_id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    phone_number INTEGER,
+    national_code INTEGER
+);
+CREATE TABLE IF NOT EXISTS Shareholder_Brand(
+  ShB_id SERIAL PRIMARY KEY,
+  shareholder_id INTEGER,
+  brand_id INTEGER
+);
+
