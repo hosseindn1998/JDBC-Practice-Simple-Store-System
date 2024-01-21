97a6b3b033c159913fe8dd1a2b83ac939bb80ed6
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-21 11:02:39 +0330

    Entity : add Shareholder class and codes

--- src/main/java/Entity/Shareholder.java
+++ src/main/java/Entity/Shareholder.java
@@ -1,4 +1,22 @@
 package Entity;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Shareholder {
+    private int id;
+    private String name;
+    private int phoneNumber;
+    private int national_code;
+
+    public Shareholder(String name, int phoneNumber, int national_code) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.national_code = national_code;
+    }
 }
+
