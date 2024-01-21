aca1c1100d22ee530ee7d3cbce8c19c764f0fa17
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-21 10:29:47 +0330

    Usre Repository : add user method

---
+++ src/main/java/Repository/UserRepository.java
@@ -0,0 +1,34 @@
+package Repository;
+
+import Entity.User;
+import lombok.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+@ToString
+public class UserRepository  {
+    private final Connection connection;
+    public UserRepository(Connection connection){
+        this.connection=connection;
+    }
+
+    public int addUser(User user) throws SQLException {
+
+        String addUserQuery = "INSERT INTO Users(name,username,email,password) VALUES(?,?,?,?); ";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(addUserQuery);
+
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getUserName());
+        preparedStatement.setString(3, user.getEmail());
+        preparedStatement.setString(4, user.getEmail());
+
+        int result = preparedStatement.executeUpdate();
+        return result;
+
+    }
+}
