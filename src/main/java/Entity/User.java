f0d842b8acf6369c59269a27b3b0a380c80c4ddc
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-22 18:47:53 +0330

    add user noIdConstructor

--- src/main/java/Entity/User.java
+++ src/main/java/Entity/User.java
@@ -16,4 +16,11 @@ public class User {
     private String userName;
     private String email;
     private String password;
+
+    public User(String name, String userName, String email, String password) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
 }
