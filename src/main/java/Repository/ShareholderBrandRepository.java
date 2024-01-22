03a30a46804e1e7d4d6b9a1ffc270fef07a1162f
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-22 17:12:26 +0330

    ShareholderBrandRepository fields and constructor

--- src/main/java/Repository/ShareholderBrandRepository.java
+++ src/main/java/Repository/ShareholderBrandRepository.java
@@ -1,4 +1,20 @@
 package Repository;
 
+import lombok.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+@ToString
 public class ShareholderBrandRepository {
-}
+    private final Connection connection;
+
+    public ShareholderBrandRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+
