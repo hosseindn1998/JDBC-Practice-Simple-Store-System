fdd95fc0a2c4d6511096293b9a2e005539dd6db1
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-22 11:06:05 +0330

    add fields and constructor

--- src/main/java/Repository/BrandRepository.java
+++ src/main/java/Repository/BrandRepository.java
@@ -1,5 +1,16 @@
 package Repository;
+import lombok.*;
+
+import java.sql.Connection;
+
+@Getter
+@Setter
+@ToString
 
 public class BrandRepository {
+private final Connection connection;
+public BrandRepository(Connection connection){
+    this.connection=connection;
+}
 
 }
