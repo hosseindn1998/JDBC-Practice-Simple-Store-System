85b6626adc63a09adf18070f7bb2562acceb305e
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-22 19:11:39 +0330

    add UserService class file and fields and constructors

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,4 +1,24 @@
 package Service;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+import Repository.UserRepository;
+import Entity.User;
+import lombok.*;
+
+@Getter
+@Setter
+
 public class UserService {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository =userRepository;
+    }
+
+
+
 }
