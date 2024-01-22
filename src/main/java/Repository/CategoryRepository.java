f3f1b3ed4dbc1855b86665f0475d5324aca4f5e0
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-22 14:20:36 +0330

    CategoryRepository : add connection and constructor

--- src/main/java/Repository/CategoryRepository.java
+++ src/main/java/Repository/CategoryRepository.java
@@ -1,5 +1,14 @@
 package Repository;
 
+import lombok.*;
+
+import java.sql.Connection;
+@Getter
+@Setter
 public class CategoryRepository {
+private final Connection connection;
+public CategoryRepository(Connection connection){
+    this.connection=connection;
+}
 
 }
