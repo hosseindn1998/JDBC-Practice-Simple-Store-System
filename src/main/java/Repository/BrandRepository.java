6bc61a41f1596584c9c8fab490a15e3caaf1aec1
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-22 13:24:11 +0330

    BrandRepository : add brand method

--- src/main/java/Repository/BrandRepository.java
+++ src/main/java/Repository/BrandRepository.java
@@ -1,7 +1,9 @@
 package Repository;
 import lombok.*;
-
+import Entity.Brand;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Getter
 @Setter
@@ -12,5 +14,13 @@ private final Connection connection;
 public BrandRepository(Connection connection){
     this.connection=connection;
 }
-
+public int addBrand(Brand brand)throws SQLException {
+    String addBrandQuery="INSERT INTO brand(name,website,description)VALUES (?,?,?);";
+    PreparedStatement preparedStatement=connection.prepareStatement(addBrandQuery);
+    preparedStatement.setString(1,brand.getName());
+    preparedStatement.setString(2,brand.getWebsite());
+    preparedStatement.setString(3,brand.getDescription());
+    int result=preparedStatement.executeUpdate();
+    return result;
+}
 }
