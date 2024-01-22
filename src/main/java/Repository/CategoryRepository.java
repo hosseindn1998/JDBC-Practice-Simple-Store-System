b2fc7ed79cc2b01004c1ce9de4e26b5cdbb66b91
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-22 14:53:58 +0330

    CategoryRepository : add addcategory method

--- src/main/java/Repository/CategoryRepository.java
+++ src/main/java/Repository/CategoryRepository.java
@@ -3,6 +3,9 @@ package Repository;
 import lombok.*;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import Entity.Category;
 @Getter
 @Setter
 public class CategoryRepository {
@@ -10,5 +13,12 @@ private final Connection connection;
 public CategoryRepository(Connection connection){
     this.connection=connection;
 }
-
+public int addCategory (Category category)throws SQLException {
+    String addCategoryQuery="INSERT INTO categories(name,description)VALUES (?,?)";
+    PreparedStatement preparedStatement=connection.prepareStatement(addCategoryQuery);
+    preparedStatement.setString(1,category.getName());
+    preparedStatement.setString(2,category.getDescription());
+    int result= preparedStatement.executeUpdate();
+    return result;
+}
 }
