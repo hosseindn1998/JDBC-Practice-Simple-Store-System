54b86b2e89540d0b1268b28fdf502a6db967c823
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 01:06:55 +0330

    CategoryService : add editCategory method

--- src/main/java/Service/CategoryService.java
+++ src/main/java/Service/CategoryService.java
@@ -21,7 +21,7 @@ public class CategoryService {
         int result= categoryRepository.addCategory(category);
         if(result==1) System.out.println(" category added Successfully ");
         else{
-            System.out.println("something is wrong");
+            System.out.println("Error in category adding . something is wrong");
         }
     }
     public void deleteCategory() throws SQLException {
@@ -30,7 +30,21 @@ public class CategoryService {
         int result=categoryRepository.deleteCategory(deleteCategoryId);
         if(result==1) System.out.println(" category deleted Successfully ");
         else{
-            System.out.println("something is wrong");
+            System.out.println("Error in category Deleting . something is wrong");
+        }
+    }
+    public void editCategory()throws SQLException{
+        System.out.println("please Enter CategoryId that you want edit? ");
+        int editCategoryId=scanner.nextInt();
+        System.out.println("Please enter your category name:");
+        String categoryName = scanner.nextLine();
+        System.out.println("Please enter your category description:");
+        String categoryDescription = scanner.nextLine();
+        Category category=new Category(editCategoryId,categoryName,categoryDescription);
+        int result= categoryRepository.editCategory(category);
+        if(result==1) System.out.println(" category edited Successfully ");
+        else{
+            System.out.println("Error in category Editing . something is wrong");
         }
     }
 }
