165e9b239b58c9da1bf1627aaf5d57dc6ffd0504
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 00:31:48 +0330

    CategoryService : add constructor and add addCategory method

--- src/main/java/Service/CategoryService.java
+++ src/main/java/Service/CategoryService.java
@@ -1,4 +1,27 @@
 package Service;
+import Repository.CategoryRepository;
+import Entity.Category;
+
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class CategoryService {
+    private final Scanner scanner = new Scanner(System.in);
+    private final CategoryRepository categoryRepository;
+    CategoryService(CategoryRepository categoryRepository){
+        this.categoryRepository=categoryRepository;
+    }
+    public void addCategory() throws SQLException {
+
+        System.out.println("Please enter your category name:");
+        String categoryName = scanner.nextLine();
+        System.out.println("Please enter your category description:");
+        String categoryDescription = scanner.nextLine();
+        Category category=new Category(categoryName,categoryDescription);
+        int result= categoryRepository.addCategory(category);
+        if(result==1) System.out.println(" category added Successfully ");
+        else{
+            System.out.println("something is wrong");
+        }
+    }
 }
