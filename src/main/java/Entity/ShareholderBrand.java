64fc38b4e4e63b3e1e9cfaf73f285c41c2c10653
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 22:41:16 +0330

    ShareholderBrand : add constructor without id

--- src/main/java/Entity/ShareholderBrand.java
+++ src/main/java/Entity/ShareholderBrand.java
@@ -10,6 +10,12 @@ public class ShareholderBrand {
     private int shareholderBrandId;
     private int shareholderId;
     private int brandId;
+
+    public ShareholderBrand(int shareholderId, int brandId) {
+        this.shareholderId=shareholderId;
+        this.brandId=brandId;
+    }
+
     public void setShareholderBrand(int shareholderId,int brandId){
     this.shareholderId=shareholderId;
     this.brandId=brandId;
