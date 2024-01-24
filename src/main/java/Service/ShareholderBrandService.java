ec236b33c7cafc1a13a288b2557d28ce41cbfb9d
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 22:35:23 +0330

    ShareholderBrandService : add deleteShareholderbrand relation

--- src/main/java/Service/ShareholderBrandService.java
+++ src/main/java/Service/ShareholderBrandService.java
@@ -23,11 +23,21 @@ public class ShareholderBrandService {
         int brandId = scanner.nextInt();
         ShareholderBrand shareholderBrand=new ShareholderBrand();
         int result= shareholderBrandRepository.addShareholderBrand(shareholderBrand);
-        if(result==1) System.out.println(" product added Successfully ");
+        if(result==1) System.out.println(" shareholder brand relation added Successfully ");
         else{
-            System.out.println("Error in product adding . something is wrong");
+            System.out.println("Error in shareholder brand relating . something is wrong");
         }
     }
+    public void deleteProduct() throws SQLException {
+        System.out.println("please Enter shareholder brand relation Id that you want delete? ");
+        int deleteShareholderBrandId=scanner.nextInt();
+        int result=shareholderBrandRepository.deleteShareholderBrand(deleteShareholderBrandId);
+        if(result==1) System.out.println(" shareholder brand relation deleted Successfully ");
+        else{
+            System.out.println("Error in shareholder brand relation Deleting . something is wrong");
+        }
+    }
+
 
 
 }
