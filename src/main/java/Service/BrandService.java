4e57245fae05951251f87444662e896ccf1a7e97
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 21:12:22 +0330

    BrandService : add addBrand method

--- src/main/java/Service/BrandService.java
+++ src/main/java/Service/BrandService.java
@@ -1,4 +1,32 @@
 package Service;
+import Entity.Brand;
+import Entity.Category;
+import Entity.Shareholder;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+import Repository.BrandRepository;
 
 public class BrandService {
+    private final BrandRepository brandRepository;
+    private final Scanner scanner=new Scanner(System.in);
+    public BrandService(BrandRepository brandRepository) {
+        this.brandRepository=brandRepository;
+    }
+    public void addBrand() throws SQLException {
+
+        System.out.println("Please enter your Brand name:");
+        String brandName = scanner.nextLine();
+        System.out.println("Please enter your Brand website:");
+        String brandWebsite = scanner.nextLine();
+        System.out.println("Please enter your category description:");
+        String brandDescription = scanner.nextLine();
+        Brand brand=new Brand(brandName,brandWebsite,brandDescription);
+        int result= brandRepository.addBrand(brand);
+        if(result==1) System.out.println(" brand added Successfully ");
+        else{
+            System.out.println("Error in brand adding . something is wrong");
+        }
+    }
+
 }
