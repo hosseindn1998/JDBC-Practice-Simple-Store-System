c9fd7af57ea654296cbaa06b692ffc7eaaeac67c
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 22:23:34 +0330

    add shareholderBrand in Entity codes setter getter

--- src/main/java/Entity/ShareholderBrand.java
+++ src/main/java/Entity/ShareholderBrand.java
@@ -1,4 +1,17 @@
 package Entity;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShareholderBrand {
+    private int shareholderBrandId;
+    private int shareholderId;
+    private int brandId;
+    public void setShareholderBrand(int shareholderId,int brandId){
+    this.shareholderId=shareholderId;
+    this.brandId=brandId;
+    }
 }
