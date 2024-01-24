d13e2023b39edf22d0bf282a7d2978251e0f511d
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 22:42:22 +0330

    ShareholderBrandService : add editeshareholder brand method

--- src/main/java/Service/ShareholderBrandService.java
+++ src/main/java/Service/ShareholderBrandService.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class ShareholderBrandService {
-    private final Scanner scanner=new scanner(System.in);
+    private final Scanner scanner=new Scanner(System.in);
 
     private final ShareholderBrandRepository shareholderBrandRepository;
     public ShareholderBrandService(ShareholderBrandRepository shareholderBrandRepository) {
@@ -21,14 +21,14 @@ public class ShareholderBrandService {
         int shareholderId = scanner.nextInt();
         System.out.println("Please enter your brand id:");
         int brandId = scanner.nextInt();
-        ShareholderBrand shareholderBrand=new ShareholderBrand();
+        ShareholderBrand shareholderBrand=new ShareholderBrand(shareholderId,brandId);
         int result= shareholderBrandRepository.addShareholderBrand(shareholderBrand);
         if(result==1) System.out.println(" shareholder brand relation added Successfully ");
         else{
             System.out.println("Error in shareholder brand relating . something is wrong");
         }
     }
-    public void deleteProduct() throws SQLException {
+    public void deleteShareholderBrand() throws SQLException {
         System.out.println("please Enter shareholder brand relation Id that you want delete? ");
         int deleteShareholderBrandId=scanner.nextInt();
         int result=shareholderBrandRepository.deleteShareholderBrand(deleteShareholderBrandId);
@@ -37,7 +37,20 @@ public class ShareholderBrandService {
             System.out.println("Error in shareholder brand relation Deleting . something is wrong");
         }
     }
-
+    public void editShareholderBrand()throws SQLException{
+        System.out.println("please Enter shareholderBrand relation Id that you want edit? ");
+        int editShareholderId=scanner.nextInt();
+        System.out.println("Please enter your shareholder id:");
+        int shareholderId = scanner.nextInt();
+        System.out.println("Please enter your brand id:");
+        int brandId = scanner.nextInt();
+        ShareholderBrand shareholderBrand=new ShareholderBrand(editShareholderId,shareholderId,brandId);
+        int result= shareholderBrandRepository.addShareholderBrand(shareholderBrand);
+        if(result==1) System.out.println(" shareholder brand relation added Successfully ");
+        else{
+            System.out.println("Error in shareholder brand relating . something is wrong");
+        }
+    }
 
 
 }
