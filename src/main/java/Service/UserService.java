7550c355f4cc1a192e35b392d1a12830bfe38230
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 19:48:14 +0330

    import Validation class

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -9,6 +9,7 @@ import java.util.regex.*;
 
 import Repository.UserRepository;
 import Entity.User;
+import Utility.Validation;
 import lombok.*;
 
 @Getter
@@ -36,17 +37,17 @@ public class UserService {
         do {
             System.out.println("Enter username :");
             machedUserName = scanner.next();
-        } while (!isValidUsername(machedUserName));
+        } while (!Validation.isValidUsername(machedUserName));
 
         do {
             System.out.println("Enter email :");
             machedEmail = scanner.next();
-        }while (!isValidEmail(machedEmail));
+        }while (!Validation.isValidEmail(machedEmail));
 
         do {
             System.out.println("Enter password :");
             machedPassword = scanner.next();
-        } while (!isValidPassword(machedPassword));
+        } while (!Validation.isValidPassword(machedPassword));
 
 
         User user = new User(name, machedUserName, machedEmail, machedPassword);
@@ -59,48 +60,7 @@ public class UserService {
 
     }
 
-    public static boolean isValidEmail(String email) {
 
-        String regex = "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@"
-                + "[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$";;
-
-        Pattern pattern = Pattern.compile(regex);
-        if (email == null) {
-            return false;
-        }
-        Matcher matcher = pattern.matcher(email);
-        return matcher.matches();
-    }
-    public static boolean isValidPassword(String password) {
-
-        String regex = "^(?=.*[0-9])"
-                + "(?=.*[a-z])(?=.*[A-Z])"
-                + "(?=.*[@#$%^&+=])"
-                + "(?=\\S+$).{8,20}$";
-
-        Pattern pattern = Pattern.compile(regex);
-        if (password == null) {
-            return false;
-        }
-
-        Matcher matcher = pattern.matcher(password);
-
-        return matcher.matches();
-    }
-
-    public static boolean isValidUsername(String username) {
-
-        String regex = "^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$";
-
-        Pattern pattern = Pattern.compile(regex);
-        if (username == null) {
-            return false;
-        }
-
-        Matcher matcher = pattern.matcher(username);
-
-        return matcher.matches();
-    }
 
     public void signIn() throws SQLException {
 
