571a27cac9c64cb82622f3ef2ad1c532372cd1e9
hosseindn1998 <hosseindn1998@gmail.com>
2024-01-24 19:47:53 +0330

    Validation : add isValidUsername isValidPassword and isValidEmail methods

--- src/main/java/Utility/Validation.java
+++ src/main/java/Utility/Validation.java
@@ -1,4 +1,49 @@
 package Utility;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Validation {
+    public static boolean isValidEmail(String email) {
+
+        String regex = "^(?=.{1,64}@)[\\p{L}0-9_-]+(\\.[\\p{L}0-9_-]+)*@"
+                + "[^-][\\p{L}0-9-]+(\\.[\\p{L}0-9-]+)*(\\.[\\p{L}]{2,})$";;
+
+        Pattern pattern = Pattern.compile(regex);
+        if (email == null) {
+            return false;
+        }
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    public static boolean isValidPassword(String password) {
+
+        String regex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        if (password == null) {
+            return false;
+        }
+
+        Matcher matcher = pattern.matcher(password);
+
+        return matcher.matches();
+    }
+
+    public static boolean isValidUsername(String username) {
+
+        String regex = "^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$";
+
+        Pattern pattern = Pattern.compile(regex);
+        if (username == null) {
+            return false;
+        }
+
+        Matcher matcher = pattern.matcher(username);
+
+        return matcher.matches();
+    }
 }
